fadd8419583c45411baa92f4b2253ba05f72b43b
Justin Ross <justinross102@gmail.com>
2023-09-15 14:47:51 -0600

    update BikeShareAnalysis.R to include linear regression

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -9,24 +9,54 @@ library(vroom)
 library(patchwork)
 
 # Read in the Data
-bikes <- vroom("./train.csv")
+bikes <- vroom("./train.csv") %>% 
+  select(-casual, -registered)
+test_data <- vroom("./test.csv")
 
 # cleaning ----------------------------------------------------------------
 
-# convert variables to factors
-bikes$season <- as.factor(bikes$season)
-bikes$holiday <- as.factor(bikes$holiday)
-bikes$workingday <- as.factor(bikes$workingday)
-bikes$weather <- as.factor(bikes$weather)
 
 # feature engineering -----------------------------------------------------
 
 my_recipe <- recipe(count ~ ., bikes) %>% 
-  step_date(datetime, features = "dow") %>% # gets day of week from datetime
-  step_rm(casual, registered, atemp) %>%  # remove casual and registered columns
-  step_replace("weather", replace = list(weather = 4), with = 3) %>% # turn 4s into 3s
-  step_select(datetime, datetime_dow, season, holiday, workingday,
-              weather, temp, humidity, windspeed, count) # reorder columns
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) %>% # turn 4s into 3s
+  step_num2factor(season, levels=c("spring", "summer", "fall", "winter")) %>%
+  step_num2factor(weather, levels=c("partly_cloudy", "misty", "rainy")) %>%
+  step_mutate(holiday=factor(holiday, levels=c(0,1), labels=c("no", "yes"))) %>%
+  step_mutate(workingday=factor(workingday,levels=c(0,1), labels=c("no", "yes")))
 
 prepped_recipe <- prep(my_recipe)
 bake(prepped_recipe, bikes)
+bake(prepped_recipe, test_data)
+
+
+# linear regression -------------------------------------------------------
+
+# set up linear regression model
+my_mod <- linear_reg() %>% 
+  set_engine("lm")
+
+bike_workflow <- workflow() %>% 
+  add_recipe(my_recipe) %>% 
+  add_model(my_mod) %>% 
+  fit(data = bikes)
+
+# use fit to predict
+bikes_predictions <- predict(bike_workflow,
+                             new_data = test_data)
+
+# round negative values to zero
+bikes_predictions[bikes_predictions < 0] <- 0
+
+# create dataset from test "datetime" and corresponding predictions
+predictions <- data.frame(test_data$datetime, bikes_predictions)
+colnames(predictions) = c("datetime", "count")
+predictions$datetime <- as.character(predictions$datetime)
+
+write_csv(predictions, "predictions.csv")
+
+
+
+
+
+
