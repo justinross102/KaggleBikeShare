b3f3d2f4c14157877b48e9c146754bccbb3b2595
Justin Ross <justinross102@gmail.com>
2023-09-18 13:33:56 -0600

    add poisson regression to analysis

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -7,27 +7,31 @@ library(tidyverse)
 library(tidymodels)
 library(vroom)
 library(patchwork)
+library(poissonreg)
 
 # Read in the Data
-bikes <- vroom("./train.csv") %>% 
-  select(-casual, -registered)
+bikes <- vroom("./train.csv")
 test_data <- vroom("./test.csv")
 
 # cleaning ----------------------------------------------------------------
 
+# Remove casual and registered because we can't use them to predict
+bikes <- bikes %>%
+  select(-casual, - registered)
 
 # feature engineering -----------------------------------------------------
 
-my_recipe <- recipe(count ~ ., bikes) %>% 
-  step_mutate(weather = ifelse(weather == 4, 3, weather)) %>% # turn 4s into 3s
-  step_num2factor(season, levels=c("spring", "summer", "fall", "winter")) %>%
-  step_num2factor(weather, levels=c("partly_cloudy", "misty", "rainy")) %>%
+my_recipe <- recipe(count~., data=bikes) %>%
+  step_mutate(weather=ifelse(weather==4, 3, weather)) %>% #Relabel weather 4 to 3
+  step_mutate(weather=factor(weather, levels=1:3, labels=c("sunny", "mist", "rain"))) %>%
+  step_mutate(season=factor(season, levels=1:4, labels=c("spring", "summer", "fall", "winter"))) %>%
   step_mutate(holiday=factor(holiday, levels=c(0,1), labels=c("no", "yes"))) %>%
-  step_mutate(workingday=factor(workingday,levels=c(0,1), labels=c("no", "yes")))
-
+  step_mutate(workingday=factor(workingday,levels=c(0,1), labels=c("no", "yes"))) %>%
+  step_time(datetime, features="hour") %>%
+  step_rm(datetime)
 prepped_recipe <- prep(my_recipe)
-bake(prepped_recipe, bikes)
-bake(prepped_recipe, test_data)
+bake(prepped_recipe, new_data = bikes) #Make sure recipe work on train
+bake(prepped_recipe, new_data = bikes) #Make sure recipe works on test
 
 
 # linear regression -------------------------------------------------------
@@ -41,20 +45,41 @@ bike_workflow <- workflow() %>%
   add_model(my_mod) %>% 
   fit(data = bikes)
 
-# use fit to predict
-bikes_predictions <- predict(bike_workflow,
-                             new_data = test_data)
+# Look at the fitted LM model
+extract_fit_engine(bike_workflow) %>%
+  summary()
 
-# round negative values to zero
-bikes_predictions[bikes_predictions < 0] <- 0
+# Get Predictions for test set AND format for Kaggle submission
+predictions <- predict(bike_workflow, new_data = test_data) %>%
+  bind_cols(., test_data) %>% # Bind predictions with test data
+  select(datetime, .pred) %>% # only keep datetime and predictions
+  rename(count=.pred) %>% # rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% # round negatives up to zero
+  mutate(datetime=as.character(format(datetime))) # needed for Kaggle submission
 
-# create dataset from test "datetime" and corresponding predictions
-predictions <- data.frame(test_data$datetime, bikes_predictions)
-colnames(predictions) = c("datetime", "count")
-predictions$datetime <- as.character(predictions$datetime)
+# Write predictions to CSV file
+vroom_write(x=predictions, file="./test_predictions.csv", delim=",")
 
-write_csv(predictions, "predictions.csv")
 
+# poisson regression ------------------------------------------------------
+
+pois_mod <- poisson_reg() %>% 
+  set_engine("glm") # generalized linear model
+
+bike_pois_workflow <- workflow() %>% 
+  add_recipe(my_recipe) %>% 
+  add_model(pois_mod) %>% 
+  fit(data = bikes)
+
+# Get Predictions for test set AND format for Kaggle submission
+pois_predictions <- predict(bike_pois_workflow, new_data = test_data) %>%
+  bind_cols(., test_data) %>% # Bind predictions with test data
+  select(datetime, .pred) %>% # only keep datetime and predictions
+  rename(count=.pred) %>% # rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% # round negatives up to zero
+  mutate(datetime=as.character(format(datetime))) # needed for Kaggle submission
+
+vroom_write(x = pois_predictions, file="./poisson_predictions.csv", delim=",")
 
 
 
